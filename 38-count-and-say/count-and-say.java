4742a13034bccf57a1d7a55959f327a23d3a57a8
yashibapaics22 <122154320+yashibapaics22@users.noreply.github.com>
2024-01-27 19:47:19 +0530

    Time: 2 ms (90.44%) | Memory: 41.5 MB (44.29%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,28 +1,32 @@
 class Solution {
     public String countAndSay(int n) {
-      String s="1";
-      for (int i=2;i<=n;i++){
-          s=count(s);
-      } 
-      return s; 
+        String s="1";
+        for(int i=2;i<=n;i++){
+          s=countAndAdd(s);
+        }
+        return s;
+        
     }
-    public static String count (String s){
-        StringBuilder str= new StringBuilder();
-        char c= s.charAt(0);
-        int count =1;
-        for (int i=1;i<s.length();i++){
-            if (s.charAt(i)==c){
-                count++;
-            }
+    public String countAndAdd(String s)
+    {
+        StringBuilder str=new StringBuilder();
+        char c=s.charAt(0);
+       int count=1;
+
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)==c) count++;
+
             else{
                 str.append(count);
                 str.append(c);
                 c=s.charAt(i);
-                count =1;
+                count=1;
             }
         }
         str.append(count);
         str.append(c);
+     
         return str.toString();
     }
 }
