3a4e0e956cd37ecf0fa292a9839820fdeb9f93ff
yashibapaics22 <122154320+yashibapaics22@users.noreply.github.com>
2024-04-17 20:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (58.12%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count=0;
+      for (int i=1;i<nums.length && count<=1 ;i++){
+        if (nums[i-1]>nums[i]){
+            count++;
+            if (i>=2 && nums[i-2]>nums[i])
+            nums[i]=nums[i-1];
+            else
+            nums[i-1]=nums[i];
+        }
+    }
+    return count<=1;
+    }
+}
