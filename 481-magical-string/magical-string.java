f1db905cd1c61398e4ac809dd141ec1a6f897a6b
yashibapaics22 <122154320+yashibapaics22@users.noreply.github.com>
2024-05-27 17:44:59 +0530

    Time: 11 ms (47.21%) | Memory: 41.7 MB (96.27%) - LeetSync

--- 481-magical-string/magical-string.java
+++ 481-magical-string/magical-string.java
@@ -1,25 +1,23 @@
 class Solution {
     public int magicalString(int n) {
-    if (n<=0)
-    return 0;
     if (n<=3)
     return 1;
     StringBuilder s= new StringBuilder("122");
     int ptr=2;
-    int count=1;
+    int count=0;
     while(s.length()<n){
         int num=s.charAt(ptr)-'0';
         char toadd= (s.charAt(s.length()-1)=='1')?'2':'1';
-        if (toadd=='1')
-        count+=num;
-        s.append(String.valueOf(toadd).repeat(num));
+        while(num>0){
+            s.append(toadd);
+            num--;
+        }
         ptr++;
     }
-    for (int i = n; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
-                count--;
-            }
-        }
+    for (int i=0;i<n;i++){
+        if (s.charAt(i)=='1')
+        count++;
+    }
     return count;
     }
 }
