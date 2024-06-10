c994ab5c7f16d019d7aa3d7286c54f06ff8fb832
yashibapaics22 <122154320+yashibapaics22@users.noreply.github.com>
2024-06-10 18:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (81.90%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // n tak me sqrt of n hi square root honge 
+        // question is based on factors .... 
+     return (int)Math.sqrt(n);   
+    }
+}
