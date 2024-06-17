a384985d7a140a6bab9550ea6227745101ab9113
yashibapaics22 <122154320+yashibapaics22@users.noreply.github.com>
2024-06-17 18:53:56 +0530

    Time: 179 ms (37.96%) | Memory: 45.1 MB (27.78%) - LeetSync

--- 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.java
+++ 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countTriplets(int[] nums) {
     int temp = 1 << 16;
-        int[] count = new int[65535];
+        int[] count = new int[temp];
         for (int num : nums) {
             for (int n : nums) {
                 count[num & n]++;
@@ -9,7 +9,7 @@ class Solution {
         }
         int res = 0;
         for (int num : nums) {
-            for (int i = 0; i < 65535; i++) {
+            for (int i = 0; i < temp; i++) {
                 if ((num & i) == 0) {
                     res += count[i];
                 }
