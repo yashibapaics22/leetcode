633d6bc6c9016f47f5567d3c9b6aad7e06507310
Yashi Bajpai <122154320+yashibapaics22@users.noreply.github.com>
2024-07-11 22:37:40 +0530

    Time: 2 ms (60.36%) | Memory: 58.3 MB (53.34%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -8,34 +8,21 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode swapNodes(ListNode head, int k) {
-        // Convert linked list to an ArrayList
-        ListNode temp = head;
-        List<Integer> ll = new ArrayList<>();
-        while (temp != null) {
-            ll.add(temp.val);
-            temp = temp.next;
+class Solution{
+    public ListNode swapNodes(ListNode head, int k){
+        ListNode curr= head;
+        for(int i=1;i<k;i++){
+            curr=curr.next;
         }
-
-        // Swap the k-th node from the beginning and the k-th node from the end
-        int n = ll.size();
-        swap(ll, k - 1, n - k);
-
-        // Convert the ArrayList back to a linked list
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        for (int val : ll) {
-            current.next = new ListNode(val);
-            current = current.next;
-        }
-
-        return dummy.next;
-    }
-
-    public static void swap(List<Integer> ll, int a, int b) {
-        int temp = ll.get(a);
-        ll.set(a, ll.get(b));
-        ll.set(b, temp);
-    }
+        ListNode a= curr;
+        ListNode b= head;
+        while(curr!=null && curr.next!=null){
+            curr=curr.next;
+            b=b.next;
+        }   
+        int temp= a.val;
+        a.val=b.val;
+        b.val=temp;
+        return head;
+    } 
 }
