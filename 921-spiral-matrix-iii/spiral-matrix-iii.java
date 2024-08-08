bdd095d03745a4680cfdf90ca818fce2add3994b
Yashi Bajpai <122154320+yashibapaics22@users.noreply.github.com>
2024-08-08 22:58:21 +0530

    Time: 4 ms (81.31%) | Memory: 45 MB (72.43%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,40 +1,19 @@
-import java.util.*;
-
 class Solution {
-    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int numSteps = 1;
-        int totalCells = rows * cols;
-        List<int[]> result = new ArrayList<>();
-        int r = rStart, c = cStart;
-        int d = 0;
-
-        while (result.size() < totalCells) {
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < numSteps; j++) {
-                    if (0 <= r && r < rows && 0 <= c && c < cols) {
-                        result.add(new int[]{r, c});
-                    }
-                    if (result.size() == totalCells) {
-                        return convertListToArray(result);
-                    }
-                    r += directions[d][0];
-                    c += directions[d][1];
-                }
-                d = (d + 1) % 4;
+    public int[][] spiralMatrixIII(int R, int C, int x, int y) {
+         int[][] res = new int[R * C][2];
+        int dx = 0, dy = 1, n = 0, tmp;
+        for (int j = 0; j < R * C; ++n) {
+            for (int i = 0; i < n / 2 + 1; ++i) {
+                if (0 <= x && x < R && 0 <= y && y < C)
+                    res[j++] = new int[] {x, y};
+                x += dx;
+                y += dy;
             }
-            numSteps++;
+            tmp = dx;
+            dx = dy;
+            dy = -tmp;
         }
-
-        return convertListToArray(result);
-    }
-
-    private int[][] convertListToArray(List<int[]> list) {
-        int[][] array = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
-            array[i] = list.get(i);
-        }
-        return array;
+        return res;
+        
     }
 }
-   
