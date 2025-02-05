0fd861dc190927e42667bef7ac51d8bd81230193
Yashi Bajpai <122154320+yashibapaics22@users.noreply.github.com>
2025-02-05 23:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (43.91%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i=1;i<nums.length;i++){
+            if((nums[i]%2==0) && (nums[i-1]%2!=0))
+            continue;
+            else if((nums[i-1]%2==0) && (nums[i]%2!=0))
+            continue;
+            else
+            return false;
+        }
+        return true;
+    }
+}
