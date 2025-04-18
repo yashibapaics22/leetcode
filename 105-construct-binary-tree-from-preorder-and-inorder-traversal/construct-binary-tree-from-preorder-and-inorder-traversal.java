b999ab66e52caaec25cc8ebaf1e80539e46469f7
Yashi Bajpai <122154320+yashibapaics22@users.noreply.github.com>
2025-04-18 19:47:58 +0530

    Time: 1 ms (95.16%) | Memory: 44.2 MB (77.34%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,19 +16,20 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         HashMap<Integer,Integer> map= new HashMap<>();
-        for (int i=0;i<inorder.length;i++){
+        for(int i=0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
         return tree(preorder,0,preorder.length-1,inorder,0,inorder.length-1,map);
     }
-    public TreeNode tree(int[] preorder,int pstart,int pend,int [] inorder,int istart,int iend,HashMap<Integer, Integer>map){
-       if (istart>iend ||pstart>pend)
-       return null;
-       TreeNode root= new TreeNode (preorder[pstart]);
-       int iroot=map.get(root.val);
-       int left= iroot-istart;
-       root.left=tree(preorder,pstart+1,pstart+left,inorder,istart,iroot-1,map);
-       root.right=tree(preorder,pstart+left+1,pend,inorder,iroot+1,iend,map);
-       return root;
+    public static TreeNode tree(int []preorder,int pstart,int pend,int [] inorder,int istart,int iend,HashMap<Integer,Integer> map){
+        if(pstart>pend)
+        return null;
+        int rootval=preorder[pstart];
+        TreeNode root= new TreeNode (rootval);
+        int mid=map.get(root.val);
+        int leftsize=mid-istart;
+        root.left=tree(preorder,pstart+1,pstart+leftsize,inorder,istart,mid-1,map);
+        root.right=tree(preorder,pstart+leftsize+1,pend,inorder,mid+1,iend,map);
+        return root;
     }
 }
